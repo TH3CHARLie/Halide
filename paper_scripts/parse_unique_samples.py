067f0fe6c6ec8e9e9474dc05d3d6775bbe6a9dd2
Xuanda Yang <xuanday@adobe.com>
2022-07-12 17:54:37 +0000

    script updates: scatter plot with lower/upper bound, slightly update unique sample script

--- paper_scripts/parse_unique_samples.py
+++ paper_scripts/parse_unique_samples.py
@@ -1,12 +1,10 @@
 import numpy as np
 import sys
-import matplotlib.pyplot as plt
 import math
-
-PREFIX = "../apps/bilateral_grid/autotuned_samples-2021-11-09-19-46-02/"
 
 if __name__ == "__main__":
     infile = sys.argv[1]
+    prefix = sys.argv[2]
     paths = []
     with open(infile, "r") as f:
         for line in f:
@@ -17,7 +15,7 @@ if __name__ == "__main__":
     for path in paths:
         batch_id = int(path[path.find("batch") + 6 : path.find("batch") + 10])
         sample_id = int(path[path.find("sample") + 7 : path.find("sample") + 11])
-        new_path = PREFIX + f"batch_{batch_id}_0/{sample_id}/{path}"
+        new_path = prefix + f"batch_{batch_id}_0/{sample_id}/{path}"
         new_paths.append(new_path)
     print(f"num of unique samples: {len(new_paths)}")
     with open("unique_training_samples.txt", "w") as o:
---
+++ paper_scripts/scatter_with_ranges.py
@@ -0,0 +1,68 @@
+from scipy import stats
+import argparse
+import pandas as pd
+import numpy as np
+import matplotlib
+matplotlib.use("Agg")
+matplotlib.rcParams["agg.path.chunksize"] = 10000
+import matplotlib.pyplot as plt
+from matplotlib.ticker import ScalarFormatter
+from matplotlib.ticker import FormatStrFormatter
+from rsquared import rsquared, relative_loss
+
+def plot(predictions_file, app, output_dir, lower_bound, upper_bound, name):
+  predicted_label = name + " Predicted (ms)"
+  actual_label = "Actual (ms)"
+  data = pd.read_csv(predictions_file, names=[predicted_label, actual_label])
+
+  r2 = rsquared(data, predicted_label, actual_label)
+  title = "{}: Run Time Predictions ($R^2$ = {:.2f}; Loss = {:.2f})".format(app, r2, relative_loss(data, predicted_label, actual_label))
+  fig, ax = plt.subplots()
+  plt.scatter(x=predicted_label, y=actual_label, data=data, s=5, linewidth=0.05, alpha=0.5)
+
+  lower_bound_label = "Lower Bound"
+  upper_bound_label = "Upper Bound"
+  lower_bound_data = pd.read_csv(lower_bound, names=[lower_bound_label, actual_label])
+  upper_bound_data = pd.read_csv(upper_bound, names=[upper_bound_label, actual_label])
+  # print(data[predicted_label])
+  # print(lower_bound_data[lower_bound_label])
+  plt.vlines(x=data[predicted_label], ymin=lower_bound_data[lower_bound_label], ymax=upper_bound_data[upper_bound_label], colors='red', lw=0.5)
+
+
+  ax.set_title(title)
+  plt.xscale('log', base=2)
+  plt.yscale('log', base=2)
+
+  max = np.ceil(np.max(np.max(data)))
+  min = np.floor(np.min(np.min(data)))
+
+  # plt.plot([0, max], [0, max], linewidth=1)
+  ax.grid(True, alpha=0.4, linestyle='--')
+  ax.grid(True, which='minor', alpha=0.4, linestyle='--')
+
+  plt.xlabel(predicted_label)
+  plt.ylabel(actual_label)
+
+  for axis in [ax.get_xaxis(), ax.get_yaxis()]:
+    axis.set_major_formatter(FormatStrFormatter("%.1f"))
+    axis.set_minor_formatter(ScalarFormatter())
+
+    for tick in axis.get_minor_ticks():
+      tick.label.set_fontsize(5)
+
+    for tick in axis.get_major_ticks():
+      tick.label.set_fontsize(7)
+
+  filename = "{}/{}.png".format(output_dir, app)
+  plt.savefig(filename, dpi=200)
+  print("Saved scatter plot to {}".format(filename))
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--app", type=str, required=True)
+  parser.add_argument("--output", type=str, required=True)
+  parser.add_argument("--lower", type=str, required=True)
+  parser.add_argument("--upper", type=str, required=True)
+
+  args = parser.parse_args()
+  plot(args.lower, args.app, args.output, args.lower, args.upper, "Lower Bound")
